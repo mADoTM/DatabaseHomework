6cf4c541c9ef59d4f5507bb81f745db3d710302a
Максим Долженко <maxim_2003@mail.ru>
2022-11-08 11:21:36 +0300

    Gradle changes commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
 plugins {
-    id("java")
+    java
 }
-
-group = "ru.mail"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
+allprojects {
+    group = "ru.mail"
+    version = "1.0-SNAPSHOT"
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.flywaydb:flyway-core:9.6.0")
+        implementation("org.postgresql:postgresql:42.5.0")
+        implementation("org.jetbrains:annotations:23.0.0")
+
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    }
+
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
 }
